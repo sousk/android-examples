d5e9ef5bbbeaa57ecc6d0244e4d6a39fb05822ce
sou <sou.phphp@gmail.com>
2016-06-12 18:28:22 +0900

    added Firebase sample app

---
+++ ToDoApp/app/src/main/java/com/example/sou/todoapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.sou.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        // Create a new Adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // Use Firebase to populate the list
+        Firebase.setAndroidContext(this);
+        new Firebase("https://mercari-p-sou-todoapp.firebaseio.com/todoItems")
+                .addChildEventListener(new ChildEventListener() {@Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String)dataSnapshot.child("text").getValue());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String)dataSnapshot.child("text").getValue());
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
+
+
+    }
+}
